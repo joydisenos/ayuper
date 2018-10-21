0d8ed349890210de4ca126500b31a817be7f4217
Joseph Vasquez <joydisenos@gmail.com>
2018-10-21 02:02:23 -0400

    Cambios varios

--- app/Notifications/MyResetPassword.php
+++ app/Notifications/MyResetPassword.php
@@ -10,6 +10,7 @@ class MyResetPassword extends ResetPassword
     public function toMail($notifiable)
     {
         return (new MailMessage)
+            ->subject('📬 Recuperar Contraseña')
             ->line('Estás recibiendo este email porque se ha solicitado un cambio de contraseña para tu cuenta.')
             ->action('Reestablecer Contraseña', route('password.reset', $this->token))
             ->line('Si no has solicitado un cambio de contraseña, puedes ignorar o eliminar este e-mail.');
